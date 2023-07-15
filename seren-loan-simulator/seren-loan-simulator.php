4af9f6bc5cde2d0e791d412f37e54307292b5f85
Cédric <milletcedric83400@gmail.com>
2023-07-15 15:17:31 +0200

    disable debug

--- seren-loan-simulator/seren-loan-simulator.php
+++ seren-loan-simulator/seren-loan-simulator.php
@@ -12,9 +12,9 @@
 if (!defined("ABSPATH")) exit; 
 
 // DEV ONLY
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 class SerenLoanCalculator
 {
